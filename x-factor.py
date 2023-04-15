432d5eedee0b96c8b9f207f9bd5958d0e19ef14e
jamesphillipturpin <jamesphillipturpin@gmail.com>
2023-04-15 14:35:08 -0700

    Update x-factor.py
    Extensive changes were made to the x-factor.py script in order to get it to run without errors and clean up the code. The current version has considerable known problems, but it provides a proof of concept for some of the intended functionality.

--- x-factor.py
+++ x-factor.py
@@ -1,35 +1,121 @@
+"""
+Example usage: Type or past the following line to the command prompt:
+python x-factor.py buggy_script.py --args="Add 1 2"
+"""
+
+"""
+There are several functions and classes in the provided code. Here is a brief description of each:
+parse_args(): This function parses the command-line arguments and returns them as a Namespace object.
+get_default_error_message_traceback(script): This function takes a script file path and returns a default error message traceback file path. It uses the script filename to create a temporary error message traceback file path in the same directory as the script.
+get_default_revision(script): This function takes a script file path and returns a default revision file path. It uses the script filename to create a temporary revision file path in the same directory as the script.
+split_path(path): This function takes a file path and returns a tuple containing the directory name and file name components.
+OpenAI_API_Model: This class represents an OpenAI model that can generate responses to prompts. It has a generate() method that takes a prompt, number of responses, maximum tokens, temperature, and top p as arguments and returns the generated responses.
+TransformersModel: This class represents a Hugging Face model that can generate responses to prompts. It has a generate() method that takes a prompt, number of responses, maximum tokens, temperature, and top p as arguments and returns the generated responses.
+run_script(script_filepath: str, script_args_as_string: str): This function runs a script using the subprocess module of Python and returns its output.
+The script reads a file with a stack trace and applies a NLP model to suggest fixes to the code. It prompts the user with the error message, the file path, and a list of arguments passed to the script, and they can provide suggested changes to the file. The script uses two models: OpenAI and Transformers. The OpenAI model generates the text prompts and the Transformers model generates the responses. The script parses command-line arguments and uses the argparse module to do so. It also reads the contents of the stack trace file and creates a list of argument strings.
+"""
+
+# Import statements are given in alphabetical order to make organization uniquely defined.
 import argparse
-import difflib
-import json
 import os
+import openai
 import shutil
 import subprocess
 import sys
-from os import environ
+from langchain import LLMChain, OpenAI as openai_langchain, PromptTemplate
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.cohere import CohereEmbeddings
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms.openai import OpenAI as openai_llm
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, PromptTemplate, SystemMessagePromptTemplate
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.vectorstores.elastic_vector_search import ElasticVectorSearch
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from termcolor import cprint
+from typing import Any, Dict, List, Optional, Tuple
 
-from typing import List, Dict, Any, Tuple
+def parse_args() -> argparse.Namespace:
+    """
+    Parse command-line arguments. Define defaults so most arguments may be convenitently omitted.
 
-import openai
-from langchain.llms.openai import OpenAI
-from langchain import PromptTemplate, LLMChain
+    Returns:
+        A Namespace object containing the parsed arguments.
+    """
+    parser = argparse.ArgumentParser(description='Script that takes a file with a stacktrace and edits it using GPT-3')
+    parser.add_argument('script', type=str, help='script file path')
+    parser.add_argument('--args', default="", type=str, help='arguments to be used when running script')
+    parser.add_argument('--openai_api_key', default=os.environ.get('OPEN_AI_API_KEY'), help='API key for OpenAI')
+    parser.add_argument('--error_message_traceback', default=None, type=str, help='error_message_traceback_file filepath to save output and/or messages from running script')
+    parser.add_argument('--revision', default=None, type=str, help='script revision file path')
+    parser.add_argument('--verbose', action='store_true', default=False,  help='enable verbose logging')
+    parser.add_argument('--confirm', action='store_true', default=False, help="Prompts the user for confirmation before applying the suggested fix.")
+    parser.add_argument('--model', default="code-davinci-002", help="Name or path of the model.")
+    parser.add_argument('--hf', action='store_true', default=False, help="Use a Hugging Face model.")
+    args = parser.parse_args()
+    if args.error_message_traceback is None:
+        args.error_message_traceback = get_default_error_message_traceback(args.script)
+    if args.revision is None:
+        args.revision = get_default_revision(args.script)
+    print()
+    print("Results of argument parser are printed below:")
+    print(args)
+    print()
+    return args
 
-from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
+def get_default_error_message_traceback(script):
+    dirname, script_filename = os.path.split(script)
+    error_message_traceback_filename = f"{script_filename}.x-factor.error_message_traceback.temporary.txt"
+    error_message_traceback_filepath = os.path.join(dirname,  error_message_traceback_filename)
+    return error_message_traceback_filepath
 
-from error_prompter import ErrorPrompter
+def get_default_revision(script):
+    dirname, script_filename = os.path.split(script)
+    revision_filename = f"{script_filename}.x-factor.revision.temporary.txt"
+    revision_filepath = os.path.join(dirname,  revision_filename)
+    return revision_filepath
 
-@register_model('openai')
-class OpenAIModel(Model):
+def split_path(path):
+    """
+    Split a file path into its directory name and file name components.
+
+    Args:
+        path (str): The path to split.
+
+    Returns:
+        A tuple containing the directory name and file name.
+    """
+    dirname, filename = os.path.split(path)
+    return dirname, filename
+
+
+class OpenAI_API_Model(object):
     def __init__(self, model_name_or_path="code-davinci-002"):
-        self.model = OpenAI(model_name = model_name_or_path)
+        self.model = openai_llm(model_name = model_name_or_path)
+        self.chat = ChatOpenAI()
 
-    def generate(self, prompt, num_responses=1, max_tokens=50, temperature=0.5, top_p=1):
+    def generate(self, prompt:dict, num_responses=1, max_tokens=50, temperature=0.5, top_p=1):
+      try:
         llm_chain = LLMChain(prompt=prompt, llm=self.model)
+        chain = LLMChain(llm=self.model, prompt=prompt)
+      except:
+        print()
+        print("Printing prompt")
+        print(prompt)
+        print()
+        print("Printing self.model")
+        print(self.model)
+        print()
+        print("Printing llm_chain")
+        print(llm_chain)
+        print()
+      finally:
         output = llm_chain.run()
         return output
 
-
-@register_model('transformers')
-class TransformersModel(Model):
+class TransformersModel(object):
     def __init__(self, model_name_or_path):
         self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
         self.model = AutoModelForCausalLM.from_pretrained(model_name_or_path)
@@ -38,118 +124,136 @@ class TransformersModel(Model):
         generator = pipeline('text-generation', model=self.model, tokenizer=self.tokenizer)
         return generator(prompt, max_length=max_tokens, num_return_sequences=num_responses, temperature=temperature, top_p=top_p)
 
-
-class ErrorPrompterWrapper:
-    def __init__(self, model: Model, prompt_generator, confirm: bool):
-        self.model = model
-        self.prompt_generator = prompt_generator
-        self.confirm = confirm
-
-    def prompt(self, file_path: str, args: List[Any], error_message: str) -> str:
-        """
-        Prompt the user with an error message, a file path, and a list of arguments,
-        and return the suggested changes to the file.
-
-        Args:
-            file_path (str): Path to the file with the error.
-            args (List[Any]): List of arguments passed to the script.
-            error_message (str): Error message.
-
-        Returns:
-            str: Suggested changes to the file.
-        """
-        with open(file_path, "r") as f:
-            file_lines = f.readlines()
-
-        file_with_lines = []
-        for i, line in enumerate(file_lines):
-            file_with_lines.append(str(i + 1) + ": " + line)
-        file_with_lines = "".join(file_with_lines)
-
-        initial_prompt_text = self.prompt_generator(file_path, args, error_message)
-
-        prompt = (
-            initial_prompt_text +
-            "\n\n"
-            "Here is the script that needs fixing:\n\n"
-            f"{file_with_lines}\n\n"
-            "Here are the arguments it was provided:\n\n"
-            f"{args}\n\n"
-            "Here is the error message:\n\n"
-            f"{error_message}\n"
-            "Please provide your suggested changes..."
+# Run a script and return its output
+def run_script(script_filepath: str, script_args_as_string: str):
+    script_args_as_tuple = (tuple(script_args_as_string.split(" ")))
+    try:
+        result = subprocess.check_output(
+            [sys.executable, script_filepath, *script_args_as_tuple], stderr=subprocess.STDOUT
         )
+    except subprocess.CalledProcessError as e:
+        return e.output.decode("utf-8"), e.returncode
+    return result.decode("utf-8"), 0
 
-        if self.confirm:
-            response = input(f"Are you sure you want to run the following code:\n\n{prompt}\n\n[Y/n] ")
-            if response.lower() == "n":
-                return ""
+def make_prompt(prompt: str, code: str, error_message_traceback: str, dialogue_history: Optional[str] = None) -> dict:
+    #prompt=PromptTemplate(
+    #    template = "Prompt:\n{prompt}\n\nCode:\n{code}\n\nError Message Traceback:\n{error_message_traceback}",
+    #    input_variables=["prompt", "code", "error_message_traceback"],
+    #)
+    input_variables=["prompt", "code", "error_message_traceback"]
+    template="Prompt:\n{prompt}\n\nCode:\n{code}\n\nError Message Traceback:\n{error_message_traceback}"
+    template_args = (prompt, code, error_message_traceback)
+    template_kwargs = {key: arg for (key, arg) in zip(input_variables, template_args)}
+    prompt = PromptTemplate(
+        input_variables=input_variables,
+        template=template,
+    )
+    system_message_prompt = SystemMessagePromptTemplate(prompt=prompt)
+    chat_prompt_template = ChatPromptTemplate.from_messages([system_message_prompt])
+    print()
+    print ("system_message_prompt")
+    print (system_message_prompt)
+    print()
+    print ("chat_prompt_template")
+    print (chat_prompt_template)
+    print()
+    return chat_prompt_template, template_args, template_kwargs
 
-        suggestions = self.model.generate(prompt, num_responses=3, max_tokens=300, temperature=0.5, top_p=0.9)
-
-        return suggestions[0]
 
 
-def send_error_to_wrapper(file_path: str, args: List[Any], error_message: str, wrapper: ErrorPrompterWrapper) -> str:
-    return wrapper.prompt(file_path, args, error_message)
-
-def parse_args() -> argparse.Namespace:
+def save_to_file(text: str, filepath: str):
+  try:
     """
-    Parse command-line arguments.
-
-    Returns:
-        A Namespace object containing the parsed arguments.
+    This is a function that writes a given text to a specified filepath. If a file already exists at the specified filepath, the function will create a backup file with an incrementing suffix before overwriting the original file.
+    It first checks whether a file already exists at the specified filepath using the os.path.exists() function.
+    If a file already exists, it creates a backup file with an incrementing suffix. For example, if filepath is "example.txt" and a backup file already exists with the name "example.txt.bak1", it will create a new backup file with the name "example.txt.bak2", and so on.
+    It then opens the original file (filepath) in read mode and the backup file in write mode using the with open() context manager.
+    It reads the contents of the original file using f1.read() and writes them to the backup file using f2.write().
+    If a backup file was created, it is closed automatically when the with block ends.
+    The function then opens the original file (filepath) in write mode using the with open() context manager.
+    It writes the specified text to the file using f1.write().
+    The file is closed automatically when the with block ends.
     """
-    parser = argparse.ArgumentParser(description='Script that takes a file with a stacktrace and edits it using GPT-3')
-    parser.add_argument('traceback_file', type=str, help='path to the file with the stacktrace')
-    parser.add_argument('--api_key', type=str, required=True)
-    parser.add_argument('--input', type=str, help='input file path')
-    parser.add_argument('--output', type=str, help='output file path')
-    parser.add_argument('--verbose', action='store_true', help='enable verbose logging')
-    parser.add_argument('--confirm', action='store_true', default=False, help="Prompts the user for confirmation before applying the suggested fix.")
-    args = parser.parse_args()
-    return args
+    print()
+    print(filepath)
+    print(text)
+    print(filepath)
+    print()
+    if os.path.exists(filepath):
+        backup_suffix = 1
+        backup_filepath = f"{filepath}.bak{backup_suffix}"
+        while os.path.exists(backup_filepath):
+            backup_suffix += 1
+            backup_filepath = f"{filepath}.bak{backup_suffix}"
+        with open(filepath, "r") as f1, open(backup_filepath, "w") as f2:
+            f2.write(f1.read())
+    
+    with open(filepath, "w") as f1:
+        f1.write(text)
+  except Exception as e:
+    print()
+    print("Printing filepath")
+    print(filepath)
+    print()
+    raise e
 
 def main():
     args = parse_args()
-    traceback_file = args.traceback_file
-    api_key = args.api_key
-    if not api_key: api_key = envvir.load("OPENAI_API_KEY")
-    openai.api_key = api_key
-    input_file = args.input
-    output_file = args.output
+    script_filepath = args.script
+    script_args = args.args
+    error_message_traceback_filepath = args.error_message_traceback
+    revision_filepath = args.revision
+    openai_api_key = args.openai_api_key
+    openai.api_key = openai_api_key
     verbose = args.verbose
+    model_name_or_path = args.model
+    isHuggingFace = args.hf
+
+    if isHuggingFace:
+        model = TransformersModel(model_name_or_path=model_name_or_path)
+    else:
+        model = OpenAI_API_Model(model_name_or_path=model_name_or_path)
 
     if verbose:
-        print(f"Verbose logging is enabled. Input file: {input_file}. Output file: {output_file}.")
+        print(f"Verbose logging is enabled. Input file: {script_filepath}. Arguments: {args}. Traceback file: {error_message_traceback_filepath}.")
     else:
-        print(f"Input file: {input_file}. Output file: {output_file}.")
+        print(f"Input file: {script_filepath}. Arguments: {args}. Traceback file: {error_message_traceback_filepath}.")
 
-    with open(traceback_file) as f:
-        traceback_lines = f.readlines()
+    output, returncode = run_script(script_filepath, script_args)    
 
-    args_list = [input_file, output_file] if input_file and output_file else []
-    error_prompter_wrapper = ErrorPrompterWrapper(OpenAIModel(api_key), ErrorPrompter(prompt_generator=stacktrace_prompt_generator), args.confirm)
+    max_trials = 1
+    dialogue_history = list()
+    instruction = "Please improve the code based on the error messages and the intent of the code."
+    for _ in range(max_trials):
+        error_message_traceback, returncode = run_script(script_filepath, script_args)
+        save_to_file(error_message_traceback, error_message_traceback_filepath)
+        if returncode == 0:
+            cprint("Script ran successfully.", "blue")
+            print("Script Dialogue:", error_message_traceback)
+            break
+        else:
+            cprint("Script crashed. Trying to fix...", "red")
+            print("Error message:", error_message_traceback)
 
-    for line in traceback_lines:
-        error_message = line.strip()
-        file_path, line_number = get_file_path_and_line_number(error_message)
-        if not file_path:
-            continue
+            chat_prompt_template, template_args, template_kwargs = make_prompt(
+                     instruction,
+                     script_filepath, 
+                     error_message_traceback_filepath,
+                     dialogue_history
+            ) 
+            print()
+            print("Printing chat_prompt_template")
+            print(chat_prompt_template)
+            print()
+            #chain = LLMChain(llm=model, prompt=chat_prompt_template)
+            chat = ChatOpenAI(temperature=0.9)
+            chain = LLMChain(llm=chat, prompt=chat_prompt_template)
+            revision = chain.run(template_kwargs)
+        save_to_file(revision, revision_filepath)
+        dialogue_history.append(chat_prompt_template)
+        dialogue_history.append(revision)
 
-        file_path = os.path.abspath(file_path)
-        args_string = line.split(file_path)[1].strip()
-        args_list += parse_args_string(args_string)
-
-        suggested_changes = send_error_to_wrapper(file_path, args_list, error_message, error_prompter_wrapper)
-        if suggested_changes:
-            if args.confirm:
-                print(f"Suggested changes: {suggested_changes}")
-            else:
-                apply_suggested_changes(suggested_changes, file_path)
-
-        args_list = [input_file, output_file] if input_file and output_file else []
-
+        if "Error" not in error_message_traceback: break
 
 if __name__ == '__main__':
     main()
